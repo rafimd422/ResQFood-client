45fe1c2ca1cef80e3c4008075d15bdc50390dea8
rafimd422 <rafimf2222@gmail.com>
2023-11-05 20:57:47 +0600

    complated navbar using merakiUI

---
+++ src/Sharable/Nav/Nav.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+
+function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+
+  const NavRoutes = <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
+    <NavLink
+      to="/availablefood"
+      className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover-bg-gray-700"
+    >
+      Available Foods
+    </NavLink>
+    <NavLink
+      to="/browse-topics"
+      className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover-bg-gray-700"
+    >
+      Add Food
+    </NavLink>
+    <NavLink
+      to="/random-item"
+      className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover-bg-gray-700"
+    >
+      Manage My foods
+    </NavLink>
+<NavLink
+      to="/experts"
+      className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover-bg-gray-700"
+    >
+      My Food Request
+    </NavLink>
+  </div>
+
+
+  return (
+    <nav className="relative w-screen bg-white shadow dark:bg-gray-800">
+      <div className="container px-6 py-4 mx-auto">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <div className="flex items-center justify-between">
+            <a href="#">
+              <img className="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="" />
+            </a>
+            <div className="flex lg:hidden">
+
+            <button
+      onClick={toggleMenu}
+      type="button"
+      className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+      aria-label="toggle menu"
+    >
+      {!isOpen ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      )}
+    </button>
+
+            </div>
+          </div>
+          <div
+            className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${
+              isOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full'
+            }`}
+          >
+             <div className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center bg-slate-100 ${
+    isOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full '
+  }`}
+  >
+   {NavRoutes}
+
+
+
+            <div className="flex items-center mt-4 lg:mt-0">
+              <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
+                <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
+                  <img
+                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                    className="object-cover w-full h-full"
+                    alt="avatar"
+                  />
+                </div>
+                <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">Khatab wedaa</h3>
+              </button>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
