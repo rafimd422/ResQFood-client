a6b24ea6127bfc97eed46a5ab9fd4f45fae00510
rafimd422 <rafimf2222@gmail.com>
2023-11-06 09:58:32 +0600

    added error page

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom';
 import Main from './Main/Main';
 import Home from './../Pages/Home/Home';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
 
 const Root = createBrowserRouter([
     {
       path: "/",
       element: <Main/>,
+      errorElement:<ErrorPage/>,
       children:[
         {
           path:'/',
--- src/Sharable/Footer/Footer.jsx
+++ src/Sharable/Footer/Footer.jsx
@@ -226,7 +226,7 @@ const Footer = () => {
     </div>
 
     <p className="text-xs text-gray-500">
-      &copy; 2022. Company Name. All rights reserved.
+      &copy; 2023 RESQFOOD All rights reserved.
     </p>
   </div>
 </footer>
