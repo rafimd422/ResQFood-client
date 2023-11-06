cd0720b00514dc9db6873db3804f0e66de280dce
rafimd422 <rafimf2222@gmail.com>
2023-11-06 21:14:41 +0600

    complated google login authentication functionality

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,17 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import regpic from './../../assets/regpage.json'
 import { Player } from '@lottiefiles/react-lottie-player'
 import { Helmet } from 'react-helmet-async'
 import { useContext } from 'react'
 import { AuthContext } from '../../Context/OurContext'
 import Swal from 'sweetalert2'
-import { updateProfile } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, updateProfile } from 'firebase/auth'
 import auth from '../../Firebase/firebase.config'
 import load from './../../assets/errorpic.json'
 
 const Register = () => {
 
 const { createUser, loading } = useContext(AuthContext)
+
+const navigate = useNavigate()
 
 if(loading){
     return <div className='h-screen w-screen flex justify-center items-center fixed bg-white'>
@@ -63,6 +65,30 @@ createUser(email, password)
 }
 
 
+//google login
+const googleLogIn = () =>{
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+  .then(result => {
+    console.log(result.user)
+    Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Log in successfull',
+    showConfirmButton: false,
+    timer: 1500
+    })
+    navigate('/')
+  
+  }) .catch(error =>{
+    Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text:`${error.message}`,
+    })
+    window.location.reload();
+  })
+  }
 
 
   return (
@@ -119,7 +145,7 @@ createUser(email, password)
      </div>
 
      <div className="my-6 space-y-4">
-         <button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ri focus:ri border-gray-400 focus:ri">
+         <button onClick={googleLogIn} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ri focus:ri border-gray-400 focus:ri">
              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                  <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
              </svg>
